109a476721fc0e8fe05917b32bc2c42472f2ee4b
yuli_s <yuli_s@7e47f64f-3c34-0410-9091-801b9fe948be>
2007-08-28 20:44:45 +0000

    Bash script to start valgrind memory check was fixed.
    git-svn-id: http://svn.code.sf.net/p/greensql/code/trunk/greensql-fw@78 7e47f64f-3c34-0410-9091-801b9fe948be

--- mem-test.sh
+++ mem-test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-valgrind --track-fds=yes --log-file=./green-mem-log --num-callers=50 --leak-check=full --leak-resolution=high --show-reachable=yes ./green
+valgrind --track-fds=yes --log-file=./green-mem-log --num-callers=50 --leak-check=full --leak-resolution=high --show-reachable=yes ./greensql-fw -p /etc/greensql/
