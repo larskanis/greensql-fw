98f1c418a16bf8077bd95f605f94cad0b8ca5cc8
yuli_s <yuli_s@7e47f64f-3c34-0410-9091-801b9fe948be>
2007-08-28 20:26:58 +0000

    Fixed memory leek bug. Mysql resultset was not properly freed.
    git-svn-id: http://svn.code.sf.net/p/greensql/code/trunk/greensql-fw@76 7e47f64f-3c34-0410-9091-801b9fe948be

--- src/alert.cpp
+++ src/alert.cpp
@@ -87,8 +87,9 @@ agroup_get(int proxy_id, std::string & dbname,std::string & pattern)
     //logevent(STORAGE, "Records Found: %lld\n", mysql_num_rows(res) );
     if (mysql_num_rows(res) > 0)
     {
-        unsigned int i = atoi(row[0]);	
         row=mysql_fetch_row(res);
+	unsigned int i = atoi(row[0]);
+	
 	/* Release memory used to store results. */
 	mysql_free_result(res);
         return i;
