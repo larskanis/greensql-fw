bf00e6a17c1a096cea72e0a0049b93081d56eda1
yuli_s <yuli_s@7e47f64f-3c34-0410-9091-801b9fe948be>
2008-02-27 22:16:11 +0000

    Added freebsd start-up script.
    git-svn-id: http://svn.code.sf.net/p/greensql/code/trunk/greensql-fw@156 7e47f64f-3c34-0410-9091-801b9fe948be

---
+++ freebsd/greensql-fw.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+#
+
+# PROVIDE: greensql-fw
+# REQUIRE: DAEMON mysql 
+# KEYWORD: shutdown
+
+#
+# Add the following line to /etc/rc.conf to enable greensql:
+# greensql_enable (bool):	Set to "NO" by default.
+#			Set it to "YES" to enable greensql.
+#
+greensql_user=${greensql_user:-"greensql"}
+greensql_group=${greensql_group:-"greensql"}
+
+. /etc/rc.subr
+
+name="greensql"
+rcvar=`set_rcvar`
+
+load_rc_config $name
+
+: ${greensql-fw_enable="NO"}
+
+command="/usr/local/sbin/greensql-fw"
+command_args="-p /usr/local/etc/greensql/ >/dev/null 2>&1 &"
+pidfile="/var/run/greensql.pid"
+required_files="/usr/local/etc/greensql/greensql.conf"
+stop_cmd="greensql_stop"
+
+greensql_stop()
+{
+killall greensql-fw
+}
+
+run_rc_command "$1"
+
+
