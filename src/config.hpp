ee11b81d67b1944b5700bc1056afc1defe3fe9b2
yuli_s <yuli_s@7e47f64f-3c34-0410-9091-801b9fe948be>
2008-08-09 09:12:32 +0000

    Load patterns function was removed.
    git-svn-id: http://svn.code.sf.net/p/greensql/code/trunk/greensql-fw@263 7e47f64f-3c34-0410-9091-801b9fe948be

--- src/config.hpp
+++ src/config.hpp
@@ -13,7 +13,6 @@
 #include <winsock2.h>
 #endif
 #include "mysql.h"
-#include "patterns.hpp"
 
 #ifdef WIN32
 #define snprintf _snprintf
@@ -22,45 +21,44 @@
 class GreenSQLConfig
 {
 public:
-	static GreenSQLConfig * getInstance();
-	static void free();
-	bool bSupport;
-	bool bRunning;
-	bool load_db();
-	bool close_db();
-	MYSQL dbConn;
-	SQLPatterns mysql_patterns;
-	bool load_patterns(std::string & path);
-	bool load(std::string & path);
+    static GreenSQLConfig * getInstance();
+    static void free();
+    bool bSupport;
+    bool bRunning;
+    bool load_db();
+    bool close_db();
+    MYSQL dbConn;
+    bool load(std::string & path);
 
-	// risk engine factors
-	int re_block_level;
-	int re_warn_level;
+    // risk engine factors
+    int re_block_level;
+    int re_warn_level;
     int re_sql_comments;
-	int re_s_tables;
-	int re_or_token;
-	int re_union_token;
+    int re_s_tables;
+    int re_or_token;
+    int re_union_token;
     int re_var_cmp_var;
-	int re_always_true;
+    int re_always_true;
     int re_empty_password;
-	int re_multiple_queries;
+    int re_multiple_queries;
+    int re_bruteforce;
 
     int log_level;
-	std::string log_file;
+    std::string log_file;
 private:
     bool parse_db_setting(std::string & key, std::string & value);
     bool parse_re_setting(std::string & key, std::string & value);
-	bool parse_log_setting(std::string & key, std::string & value);
+    bool parse_log_setting(std::string & key, std::string & value);
     std::string sDbHost;
-	int iDbPort;
-	std::string sDbName;
-	std::string sDbUser;
-	std::string sDbPass;
+    int iDbPort;
+    std::string sDbName;
+    std::string sDbUser;
+    std::string sDbPass;
         
-	static GreenSQLConfig * _obj;
-	GreenSQLConfig();
-	~GreenSQLConfig();
-	void init();
+    static GreenSQLConfig * _obj;
+    GreenSQLConfig();
+    ~GreenSQLConfig();
+    void init();
 };
 
 #endif
