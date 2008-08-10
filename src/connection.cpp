1cec40e9825787473da4ab1b96b542992ea84815
yuli_s <yuli_s@7e47f64f-3c34-0410-9091-801b9fe948be>
2008-08-10 17:29:52 +0000

    Risk calculalation function moved to connection class.
    git-svn-id: http://svn.code.sf.net/p/greensql/code/trunk/greensql-fw@264 7e47f64f-3c34-0410-9091-801b9fe948be

--- src/connection.cpp
+++ src/connection.cpp
@@ -12,6 +12,7 @@
 #include "misc.hpp"
 #include "dbmap.hpp"
 #include "alert.hpp"
+#include "parser/parser.hpp"     // for query_risk struct
 
 
 Connection::Connection(int proxy_id)
@@ -52,44 +53,12 @@ bool Connection::check_query(std::string & query)
     logevent(SQL_DEBUG, "AFTER NORM   : %s\n", query.c_str());
     bool ret = false;
     int risk = 0;
-    
-    if (db->CanAlter() == false)
-    {
-        ret = conf->mysql_patterns.Match( SQL_ALTER,  query );
-	if (ret == true)
-	{
-            reason += "Query blocked due to the 'alter' rules.\n";
-	}
-    }
-    if (ret == false && db->CanDrop() == false)
-    {
-	ret = conf->mysql_patterns.Match( SQL_DROP,  query );
-        if (ret == true)
-        {
-            reason += "Query blocked due to the 'drop' rules.\n";
-        }
-    }
-    if (ret == false && db->CanCreate() == false)
-    {
-        ret = conf->mysql_patterns.Match( SQL_CREATE,  query );
-        if (ret == true)
-        {
-            reason += "Query blocked due to the 'create' rules.\n";
-        }
-    }
-    if (ret == false && db->CanGetInfo() == false)
-    {
-        ret = conf->mysql_patterns.Match( SQL_INFO,  query );
-        if (ret == true)
-        {
-            reason += "Query blocked due to the 'info' rules.\n";
-        }
-    }
+    ret = checkBlacklist(query, reason);
 
     if (ret == false)
     {
 	// if
-	risk = calc_risk(original_query, query, reason);
+	risk = calculateRisk(original_query, query, reason);
 	logevent(SQL_DEBUG, "RISK         : %d\n", risk);
 
 	//check if risk is low
@@ -124,5 +93,69 @@ bool Connection::check_query(std::string & query)
     }
 
     return true;
+}
+
+unsigned int Connection::calculateRisk(std::string & query,
+  std::string & pattern, std::string & reason)
+{
+    GreenSQLConfig * conf = GreenSQLConfig::getInstance();
+    unsigned int ret = 0;
+
+    struct query_risk risk;
+    query_parse(&risk, getSQLPatterns(), query.c_str());
+
+    if (conf->re_sql_comments > 0 &&
+        risk.has_comment == 1)
+    {
+        reason += "Query has comments\n";
+        logevent(DEBUG, "Query has comments\n");
+        ret += conf->re_sql_comments;
+    }
+
+    if (conf->re_s_tables > 0 &&
+        risk.has_s_table == 1)
+    {
+        reason += "Query uses sensitive tables\n";
+        logevent(DEBUG, "Query uses sensitive tables\n");
+        ret += conf->re_s_tables;
+    }
+    if (conf->re_multiple_queries > 0 &&
+        risk.has_separator == 1)
+    {
+        reason += "Multiple queries found\n";
+        logevent(DEBUG, "Multiple queries found\n");
+        ret += conf->re_multiple_queries;
+    }
+
+    if (conf->re_or_token > 0 &&
+        risk.has_or == 1)
+    {
+        reason += "Query has 'or' token\n";
+        logevent(DEBUG, "Query has 'or' token\n");
+        ret += conf->re_or_token;
+    }
+    if (conf->re_union_token > 0 &&
+        risk.has_union == 1)
+    {
+        reason += "Query has 'union' token\n";
+        logevent(DEBUG, "Query has 'union' token\n");
+        ret += conf->re_union_token;
+    }
+
+    if (conf->re_var_cmp_var > 0 &&
+        risk.has_tautology == 1)
+    {
+        reason += "Variable comparison only\n";
+        logevent(DEBUG, "Variable comparison only\n");
+        ret += conf->re_var_cmp_var;
+    }
+    if (conf->re_empty_password > 0 &&
+        risk.has_empty_pwd == 1)
+    {
+        reason += "Query has empty password expression\n";
+        logevent(DEBUG, "Query has empty password expression\n");
+        ret += conf->re_empty_password;
+    }
+    return ret;
 }
 
