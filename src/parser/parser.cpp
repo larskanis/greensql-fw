7c14247500c309be00f73d7e1a6dc9c01c221c10
yuli_s <yuli_s@7e47f64f-3c34-0410-9091-801b9fe948be>
2008-08-25 16:36:22 +0000

    memset() function was used incorectly.
    git-svn-id: http://svn.code.sf.net/p/greensql/code/trunk/greensql-fw@277 7e47f64f-3c34-0410-9091-801b9fe948be

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -20,7 +20,7 @@ int scan_buffer(const char * data);
 static bool free_sql_strings();
 static bool free_expressions();
 
-static query_risk risk;
+static query_risk risk = {0};
 static query_risk * p_risk = &risk;
 static SQLPatterns * patterns = NULL;
 
@@ -29,7 +29,8 @@ static SQLPatterns * patterns = NULL;
 int main()
 {
   char buf[1024];
-  snprintf(buf,sizeof(buf), "SELECT * FROM users WHERE laston=unix_timestamp()+10*20+fn() AND user_level>1 ORDER BY userid ASC");
+  //snprintf(buf,sizeof(buf), "SELECT * FROM users WHERE laston=unix_timestamp()+10*20+fn() AND user_level>1 ORDER BY userid ASC");
+  snprintf(buf,sizeof(buf), "SELECT * FROM users WHERE laston=1");
   scan_buffer(buf);
 
   /*
@@ -56,7 +57,7 @@ bool query_parse(struct query_risk * q_risk,
 {
   p_risk = q_risk;
   //bzero(q_risk, sizeof(struct query_risk));
-  memset(q_risk, sizeof(struct query_risk), 0);
+  memset(q_risk, 0, sizeof(struct query_risk));
   patterns = sql_patterns;
   scan_buffer(q);
   free_sql_strings();
@@ -98,19 +99,37 @@ void clb_found_table(SQLString * s)
 #endif
 }
 
+// this function check if filed is a stored variable
+// for example current_user in MySQL
 bool clb_check_true_constant(SQLString * s)
 {
   if (*(s->Dump()) == 0)
     return false;
 #ifndef PARSER_DEBUG
-  GreenSQLConfig * conf = GreenSQLConfig::getInstance();
-  if (patterns != NULL )
+  if (patterns != NULL && patterns->HasTrueConstantPatterns())
     if (patterns->Match( SQL_TRUE_CONSTANTS,  * s->GetStr() ) )
       return true;
 #endif
   return false;
 }
-	
+
+// check if the function name passed can be used to bruteforce db contents
+bool clb_check_bruteforce_function(SQLString * s )
+{
+  if (*(s->Dump()) == 0)
+    return false;
+#ifndef PARSER_DEBUG
+  if (patterns != NULL && patterns->HasBruteforcePatterns() )
+    if (patterns->Match( SQL_BRUTEFORCE_FUNCTIONS, * s->GetStr() ) )
+    {
+      p_risk->has_bruteforce_function = 1;
+      return true;
+    }
+#endif
+  return false;
+
+}
+
 void clb_found_tautology()
 {
   p_risk->has_tautology = 1;
