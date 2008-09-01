1ad573732b8f094004d61f315925b5c5c16dbf95
yuli_s <yuli_s@7e47f64f-3c34-0410-9091-801b9fe948be>
2008-09-01 17:26:52 +0000

    Adding support for learning mode.
    git-svn-id: http://svn.code.sf.net/p/greensql/code/trunk/greensql-fw@293 7e47f64f-3c34-0410-9091-801b9fe948be

--- src/dbpermobj.hpp
+++ src/dbpermobj.hpp
@@ -11,6 +11,23 @@
 #include <string>
 #include <map>
 
+enum DBBlockStatus {
+    RISK_BLOCK          = 0,      // block based on the risk calculation 
+    PRIVILEGE_BLOCK     = 1,      // block only admin and sensitive commands
+    RISK_SIMULATION     = 2,      // simulation mode for risk calculation
+    PRIVILEGE_SIMULATION= 3,      // simulation mode for sensitive commands
+    ALWAYS_BLOCK_NEW    = 4,      // block all new queries
+    LEARNING_MODE       = 10,      // automatically approve all queries
+    LEARNING_MODE_3DAYS = 11,      //    ... for 3 days
+    LEARNING_MODE_7DAYS = 12,      //    ... for 7 days
+};
+
+enum DBBlockLevel {
+    WARN                = 0,
+    BLOCKED             = 1,
+    HIGH_RISK           = 2,
+};
+
 class DBPermObj
 {
 public:
@@ -19,6 +36,8 @@ public:
                  alter_perm(false),
                  info_perm(false),
 		 block_q_perm(false),
+		 bruteforce_perm(false),
+                 block_status(RISK_BLOCK),
                  db_name("default"),
                  proxy_id(0)
     {}
@@ -30,7 +49,7 @@ public:
 		    bool _alter, bool _info, bool _block_q);
     
     bool LoadExceptions();
-    int CheckQuery(std::string & q);
+    int CheckWhitelist(std::string & q);
 	    
     bool CanCreate()
     {
@@ -52,12 +71,18 @@ public:
     {
 	    return block_q_perm;
     }
+    DBBlockStatus GetBlockStatus()
+    {
+	    return block_status;
+    }
 private: 
     bool create_perm;
     bool drop_perm;
     bool alter_perm;
     bool info_perm;
     bool block_q_perm;
+    bool bruteforce_perm;
+    DBBlockStatus block_status;
     std::string db_name;
     std::string db_type;
     unsigned int proxy_id;
