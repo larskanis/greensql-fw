248e31cf023c6ab67a69cf7e6810cb8535dfe286
yuli_s <yuli_s@7e47f64f-3c34-0410-9091-801b9fe948be>
2009-12-02 15:56:41 +0000

    merging with new version
    git-svn-id: http://svn.code.sf.net/p/greensql/code/trunk/greensql-fw@618 7e47f64f-3c34-0410-9091-801b9fe948be

--- db/mysql_create_tables.sql
+++ db/mysql_create_tables.sql
@@ -1,3 +1,30 @@
+--
+-- This file contains GreenSQL configuration database structure.
+--
+-- Step 1 - Configuration database
+-- You can create configuration database using the following
+-- MySQL command:
+-- 
+-- mysql> create database greendb;
+--
+-- Step 2 - Create MySQL user
+-- Second, create green user. It will be used to access configuration db.
+--
+-- mysql> GRANT ALL PRIVILEGES ON greendb.* TO 'green'@'%' IDENTIFIED BY 'pwd';
+--
+-- IMPORTANT: change "pwd" to other password. Don't forget to update
+-- /etc/greensql/greensql.conf file with new password value.
+--
+-- Step 3 - Flush privileges
+-- Run the following mysql command:
+--
+-- mysql> flush privileges;
+--
+-- Step 4- Create all tables
+-- Run the following shell command:
+--
+-- cat greensql-mysql-db.txt | mysql greendb
+--
 
 drop table if exists query;
 
@@ -9,7 +36,7 @@ perm           smallint unsigned NOT NULL default 1,
 db_name        char(50) NOT NULL,
 query          text NOT NULL,
 INDEX(proxyid,db_name)
-);
+) DEFAULT CHARSET=utf8;
 
 drop table if exists proxy;
 
@@ -17,17 +44,17 @@ CREATE table proxy
 (
 proxyid        int unsigned NOT NULL auto_increment primary key,
 proxyname      char(50) NOT NULL default '',
-frontend_ip    int unsigned NOT NULL default 0,
+frontend_ip    char(20) NOT NULL default '',
 frontend_port  smallint unsigned NOT NULL default 0,
 backend_server char(50) NOT NULL default '',
-backend_ip     int unsigned NOT NULL default 0,
+backend_ip     char(20) NOT NULL default '',
 backend_port   smallint unsigned NOT NULL default 0,
 dbtype         char(20) NOT NULL default 'mysql',
 status         smallint unsigned NOT NULL default '1'
-);
-insert into proxy values (0,'Default Proxy',INET_ATON('127.0.0.1'),3305,
-'localhost',INET_ATON('127.0.0.1'),3306,'mysql',1);
+) DEFAULT CHARSET=utf8;
 
+insert into proxy values (1,'Default MySQL Proxy','127.0.0.1',3305,'localhost','127.0.0.1',3306,'mysql',1);
+insert into proxy values (2,'Default PgSQL Proxy','127.0.0.1',5431,'localhost','127.0.0.1',5432,'pgsql',1);
 
 drop table if exists db_perm;
 
@@ -39,24 +66,26 @@ db_name        char(50) NOT NULL,
 perms          bigint unsigned NOT NULL default '0',
 perms2         bigint unsigned NOT NULL default '0',
 status         smallint unsigned NOT NULL default '0',
+sysdbtype      char(20) NOT NULL default 'user_db',
 status_changed datetime NOT NULL default '00-00-0000 00:00:00',
 INDEX (proxyid, db_name)
-);
+) DEFAULT CHARSET=utf8;
 
-insert into db_perm (dbpid, proxyid, db_name) values (0,0,'defaultdb');
+insert into db_perm (dbpid, proxyid, db_name, sysdbtype) values (1,0,'default mysql db', 'default_mysql');
+insert into db_perm (dbpid, proxyid, db_name, sysdbtype) values (2,0,'no-name mysql db', 'empty_mysql');
+insert into db_perm (dbpid, proxyid, db_name, sysdbtype) values (3,0,'default pgsql db', 'default_pgsql');
 
+drop table if exists admin;
 
-drop table if exists user;
-
-CREATE table user
+CREATE table admin
 (
-userid         int unsigned NOT NULL auto_increment primary key,
+adminid         int unsigned NOT NULL auto_increment primary key,
 name           char(50) NOT NULL default '',
 pwd            char(50) NOT NULL default '',
 email          char(50) NOT NULL default ''
-);
+) DEFAULT CHARSET=utf8;
 
-insert into user values(0,'admin',sha1('pwd'),'');
+insert into admin values(1,'admin',sha1('pwd'),'');
 
 drop table if exists alert;
 
@@ -67,10 +96,10 @@ agroupid            int unsigned NOT NULL default '0',
 event_time          datetime NOT NULL default '00-00-0000 00:00:00',
 risk                smallint unsigned NOT NULL default '0',
 block               smallint unsigned NOT NULL default '0',
-user                varchar(50) NULL default '',
+dbuser              varchar(50) NULL default '',
 query               text NOT NULL,
 reason              text NOT NULL
-);
+) DEFAULT CHARSET=utf8;
 
 drop table if exists alert_group;
 
@@ -83,6 +112,6 @@ update_time         datetime NOT NULL default '00-00-0000 00:00:00',
 status              smallint NOT NULL default 0,
 pattern             text NOT NULL,
 INDEX(update_time)
-);
+) DEFAULT CHARSET=utf8;
 
 
---
+++ db/pgsql_create_tables.sql
@@ -0,0 +1,91 @@
+
+drop table if exists query;
+
+CREATE table query
+(
+queryid	       SERIAL primary key,
+proxyid        int NOT NULL default '0',
+perm           smallint NOT NULL default 1,
+db_name        varchar(50) NOT NULL,
+query          text NOT NULL
+);
+
+CREATE INDEX query_idx on query(proxyid,db_name);
+
+drop table if exists proxy;
+
+CREATE table proxy
+(
+proxyid        SERIAL primary key,
+proxyname      varchar(50) NOT NULL default '',
+frontend_ip    varchar(20) NOT NULL default '',
+frontend_port  smallint NOT NULL default 0,
+backend_server varchar(50) NOT NULL default '',
+backend_ip     varchar(20) NOT NULL default '',
+backend_port   smallint NOT NULL default 0,
+dbtype         varchar(20) NOT NULL default 'mysql',
+status         smallint NOT NULL default '1'
+);
+insert into proxy values (1,'Default MySQL Proxy','127.0.0.1',3305,'localhost','127.0.0.1',3306,'mysql',1);
+insert into proxy values (2,'Default PgSQL Proxy','127.0.0.1',5431,'localhost','127.0.0.1',5432,'pgsql',1);
+select setval('proxy_proxyid_seq'::regclass,2);
+
+drop table if exists db_perm;
+
+CREATE table db_perm
+(
+dbpid          SERIAL primary key,
+proxyid        int NOT NULL default '0',
+db_name        varchar(50) NOT NULL,
+perms          bigint NOT NULL default '0',
+perms2         bigint NOT NULL default '0',
+status         smallint  NOT NULL default '0',
+sysdbtype      varchar(20) NOT NULL default 'user_db',
+status_changed TIMESTAMP
+);
+
+CREATE INDEX db_perm_idx on db_perm(proxyid, db_name);
+
+insert into db_perm (dbpid, proxyid, db_name, sysdbtype) values (1,0,'default mysql db', 'default_mysql');
+insert into db_perm (dbpid, proxyid, db_name, sysdbtype) values (2,0,'no-name mysql db', 'empty_mysql');
+insert into db_perm (dbpid, proxyid, db_name, sysdbtype) values (3,0,'default pgsql db', 'default_pgsql');
+select setval('db_perm_dbpid_seq'::regclass,3);
+
+drop table if exists admin;
+
+CREATE table admin
+(
+adminid        SERIAL PRIMARY KEY,
+name           varchar(50) NOT NULL default '',
+pwd            varchar(50) NOT NULL default '',
+email          varchar(50) NOT NULL default ''
+);
+
+insert into admin values(1,'admin','37fa265330ad83eaa879efb1e2db6380896cf639','');
+select setval('admin_adminid_seq'::regclass,1);
+
+drop table if exists alert;
+CREATE table alert
+(
+alertid             SERIAL PRIMARY KEY,
+agroupid            int default '0',
+event_time          timestamp,
+risk                smallint NOT NULL default '0',
+block               smallint NOT NULL default '0',
+dbuser              varchar(50) NULL default '',
+query               text NOT NULL,
+reason              text NOT NULL
+);
+
+drop table if exists alert_group;
+CREATE table alert_group
+(
+agroupid            SERIAL PRIMARY KEY,
+proxyid             int NOT NULL default '1',
+db_name             varchar(50) NOT NULL default '',
+update_time         timestamp,
+status              smallint NOT NULL default 0,
+pattern             text NOT NULL
+);
+
+create index alert_group_idx on alert_group(update_time);
